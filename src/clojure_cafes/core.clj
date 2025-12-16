d511afc9a6de25eac7ce4293ea553ff70979c294
emilijap11 <emilijaprvulovic3@gmail.com>
2025-12-16 14:18:30 +0100

    Add average-coffee and total-scores functions

--- src/clojure_cafes/core.clj
+++ src/clojure_cafes/core.clj
@@ -1,9 +1,11 @@
 (ns clojure-cafes.core)
 
+;; PODACI O KAFIĆIMA (immutable map-e)
 
-;; PODACI
+
 (def cafes
-  [{:name "Kafeterija"
+  [{:id 1
+    :name "Kafeterija"
     :location "Dorćol"
     :coffee 5
     :ambience 4
@@ -12,7 +14,8 @@
     :milk #{:soy :cow}
     :coffee-until 22}
 
-   {:name "Pržionica D59B"
+   {:id 2
+    :name "Pržionica D59B"
     :location "Vračar"
     :coffee 4
     :ambience 5
@@ -21,7 +24,8 @@
     :milk #{:cow :coconut}
     :coffee-until 20}
 
-   {:name "Blaznavac Café"
+   {:id 3
+    :name "Blaznavac Café"
     :location "Centar"
     :coffee 3
     :ambience 5
@@ -30,7 +34,8 @@
     :milk #{:soy :cow :coconut}
     :coffee-until 23}
 
-   {:name "Novobeogradska Pržionica"
+   {:id 4
+    :name "Novobeogradska Pržionica"
     :location "Novi Beograd"
     :coffee 5
     :ambience 3
@@ -38,82 +43,90 @@
     :noise 3
     :milk #{:cow}
     :coffee-until 19}])
-;; izvlačenje ocena kafe
-(def coffee-scores
-  (map :coffee cafes))
 
-;; kafeterije u Dorćolu
-(def dorcol-cafes
-  (filter #(= "Dorćol" (:location %)) cafes))
 
-;; prosečan ambijent
-(defn average-ambience []
-  (let [scores (map :ambience cafes)
-        total  (reduce + scores)
-        n      (count cafes)]
-    (/ total n)))
+;; USER + USER PREFERENCES
 
-;; zbirni podaci
-(def aggregated
-  (reduce
-    (fn [[cnt coffee-sum ambience-sum] e]
-      [(inc cnt)
-       (+ coffee-sum (:coffee e))
-       (+ ambience-sum (:ambience e))])
-    [0 0 0]
-    cafes))
 
-;;  prosečna ocena kafe
-(defn average-coffee []
-  (let [scores (map :coffee cafes)
-        total  (reduce + scores)
-        n      (count cafes)]
-    (/ total n)))
+(def user
+  {:id 100
+   :name "Emilija"
+   :location "Dorćol"
+   :preferences {:milk :soy
+                 :min-coffee 4
+                 :likes-quiet true
+                 :open-after 21}})
 
-;; ukupna suma svih ocena
-(defn total-scores []
-  (reduce (fn [sum cafe]
-            (+ sum (:coffee cafe) (:ambience cafe)))
-          0
-          cafes))
+;; GENERIČKE POMOĆNE FUNKCIJE
 
-(defn short-strings [strings]
-  (filter #(<= (count %) 3) strings))
 
-(defn total-chars [strings]
-  (reduce + (map count strings)))
+(defn average [values]
+  (/ (reduce + values) (count values)))
 
-(defn total-chars [strings]
-  (reduce #(+ %1 (count %2)) 0 strings))
+;; ANALIZA PODATAKA (map / filter / reduce)
 
-;;kafići koji imaju određeno mleko
-(defn cafes-with-milk [milk-type]
-  (filter #(contains? (:milk %) milk-type) cafes))
-
-;;Agregacija (BROJ, suma kafe, suma ambijenta)
-(def aggregated
-  (reduce
-    (fn [[cnt coffee-sum ambience-sum] cafe]
-      [(inc cnt)
-       (+ coffee-sum (:coffee cafe))
-       (+ ambience-sum (:ambience cafe))])
-    [0 0 0]
-    cafes))
-
-;;kafići koji služe kafu posle određenog vremena
-(defn cafes-open-after [hour]
-  (filter #(>= (:coffee-until %) hour) cafes))
-;;Sve kafeterije iz jednog mesta (parametar)
-(defn cafes-by-location [loc]
-  (filter #(= loc (:location %)) cafes))
-
-;;
 (defn average-coffee []
   (average (map :coffee cafes)))
 
 (defn average-ambience []
   (average (map :ambience cafes)))
 
-;; Score = (ocena ambijenta * 0.4)
-;      + (podudaranje kafe * 0.4)
-;      + (udaljenost * 0.2)
+(defn cafes-by-location [loc]
+  (filter #(= loc (:location %)) cafes))
+
+(defn cafes-with-milk [milk-type]
+  (filter #(contains? (:milk %) milk-type) cafes))
+
+(defn cafes-open-after [hour]
+  (filter #(>= (:coffee-until %) hour) cafes))
+
+(defn total-scores []
+  (reduce
+    (fn [sum cafe]
+      (+ sum (:coffee cafe) (:ambience cafe)))
+    0
+    cafes))
+
+
+
+;; Score = kafa,ambijent,estetika,buka ,(prilagođeno user-u)
+
+(defn cafe-score [cafe user]
+  (let [{:keys [preferences]} user
+        milk-match (if (contains? (:milk cafe) (:milk preferences)) 1 0)
+        quiet-bonus (if (and (:likes-quiet preferences)
+                             (<= (:noise cafe) 3))
+                      1
+                      0)]
+    (+ (:coffee cafe)
+       (:ambience cafe)
+       (:esthetics cafe)
+       milk-match
+       quiet-bonus)))
+
+
+;; PERSONALIZOVANA PREPORUKA
+
+
+(defn recommend-cafes [user]
+  (let [{:keys [preferences location]} user]
+    (->> cafes
+         ;; filtriranje po uslovima
+         (filter #(>= (:coffee %) (:min-coffee preferences)))
+         (filter #(contains? (:milk %) (:milk preferences)))
+         (filter #(>= (:coffee-until %) (:open-after preferences)))
+         ;; dodavanje score-a
+         (map #(assoc % :score (cafe-score % user)))
+         ;; sortiranje po score-u
+         (sort-by :score >))))
+
+
+;; PRIMER KORIŠĆENJA
+
+
+(comment
+  (average-coffee)
+  (average-ambience)
+  (cafes-by-location "Dorćol")
+  (cafes-with-milk :soy)
+  (recommend-cafes user))
